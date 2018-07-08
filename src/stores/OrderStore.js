adec421a4984971db11bdc19b904be77da5c4dc4
luan2199 <thanhluan2196@gmail.com>
2018-07-08 22:04:00 +0700

    FULL

--- src/stores/OrderStore.js
+++ src/stores/OrderStore.js
@@ -4,7 +4,7 @@ import dispatcher from "../dispatcher";
 class ProductStore extends EventEmitter {
     constructor() {
         super();
-        this.product = [{order_No:"HTC One",product_qty:"",name:"HTC One", Date:"Phone",region:"HTC",status:"" },{order_No:"iPhone",product_qty:"",name:"iPhone", Date:"Phone",region:"Apple",status:"" },{order_No:"Galaxy S8", product_qty:"",name:"Galaxy S8", Date:"Phone",region:"SamSung",status:"" },{order_No:"Dell Vostro",product_qty:"",name:"Dell Vostro", Date:"Laptop",region:"Dell",status:"" },{order_No:"Macbook",product_qty:"",name:"Macbook", Date:"Laptop",region:"Apple",status:"" },{order_No:"iPad",product_qty:"",name:"iPad", Date:"Tablet",region:"Apple",status:"" },{order_No:"Macbook Air" ,product_qty:"",name:"Macbook Air", Date:"Laptop",region:"Apple",status:"" },{order_No:"SamSung J7",product_qty:"",name:"SamSung J7", Date:"Phone",region:"SamSung",status:"" }]
+        this.product = [{order_No:"#VI1234",product_qty:"1",name:"HTC One", Date:"Phone",region:"HTC",status:"" },{order_No:"#VI1235",product_qty:"2",name:"iPhone", Date:"Phone",region:"Apple",status:"" },{order_No:"#VI1236", product_qty:"1",name:"Galaxy S8", Date:"Phone",region:"SamSung",status:"" },{order_No:"#MYS1234",product_qty:"1",name:"Dell Vostro", Date:"Laptop",region:"Dell",status:"" },{order_No:"#MYS1235",product_qty:"3",name:"Macbook", Date:"Laptop",region:"Apple",status:"" },{order_No:"#MYS1236",product_qty:"2",name:"iPad", Date:"Tablet",region:"Apple",status:"" },{order_No:"#SY1234" ,product_qty:"2",name:"Macbook Air", Date:"Laptop",region:"Apple",status:"" },{order_No:"#SY1235",product_qty:"2",name:"SamSung J7", Date:"Phone",region:"SamSung",status:"" }]
     }
 
     createProduct(name,category,brand) {
